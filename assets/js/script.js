f11ee496a372b68c0b97e861b9617cbab90918c5
Joseph Ruff <joegruff16@gmail.com>
2024-04-07 16:09:53 -0400

    Creeated a city variable to collect users input via the future form I will create and commented out how to do this with city, state, and country via Open Weathers API documentation

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,17 @@
 
 // Need to store my API key in a variable
 const APIKey = "8a4161e110974484d73136be7dd84581";
+
+// Since we want our users to be able to search by city location to find the weather
+// We need to collect the user's input to store in a variable for city
+let city;
+
+// https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={8a4161e110974484d73136be7dd84581}
+
+// What we will also want to do is specify state and country since other states and countries have the same city names
+
+// Country
+// https://api.openweathermap.org/data/2.5/weather?q={city name},{country code}&appid={API key}
+
+// State
+// https://api.openweathermap.org/data/2.5/weather?q={city name},{state code},{country code}&appid={API key}
